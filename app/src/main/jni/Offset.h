e92aff757e35c2790b8d666c53667563d2b30eb6
tojik_proof_93 <elementman50@yandex.com>
2026-02-06 03:56:56 +0500

    Update Offset.h

--- app/src/main/jni/Offset.h
+++ app/src/main/jni/Offset.h
@@ -2,29 +2,31 @@ lib l2cpp;
 Address _Transform, _GetPositionInjected, _WorldToScreenPointIntInjected, _Camera;
 
 void getAddress() {
-    l2cpp.name = "libil2cpp.so";
-    l2cpp.open();
+    lib.name = "libil2cpp.so";
+    lib.open();
 
-    // Normal Example 
-    Uclass ComponentClass = l2cpp.getClass("UnityEngine", "Component");
+    // Component
+    Uclass ComponentClass = lib.getClass("UnityEngine", "Component");
     _Transform = ComponentClass.dump("get_transform", 0);
 
-    Uclass TransformClass = l2cpp.getClass("UnityEngine", "Transform");
+    // Transform
+    Uclass TransformClass = lib.getClass("UnityEngine", "Transform");
     _GetPositionInjected = TransformClass.dump("get_position_Injected", 0);
-    if (_GetPositionInjected.get() == 0)
+    if (_GetPositionInjected.address == 0)
         _GetPositionInjected = TransformClass.dump("get_position_Injected", 1);
-    if (_GetPositionInjected.get() == 0)
+    if (_GetPositionInjected.address == 0)
         _GetPositionInjected = TransformClass.dump("get_position_Injected", 2);
-    if (_GetPositionInjected.get() == 0)
+    if (_GetPositionInjected.address == 0)
         _GetPositionInjected = TransformClass.dump("get_position_Injected", 3);
 
-    Uclass CameraClass = l2cpp.getClass("UnityEngine", "Camera");
+    // Camera
+    Uclass CameraClass = lib.getClass("UnityEngine", "Camera");
     _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 0);
-    if (_WorldToScreenPointIntInjected.get() == 0)
+    if (_WorldToScreenPointIntInjected.address == 0)
         _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 1);
-    if (_WorldToScreenPointIntInjected.get() == 0)
+    if (_WorldToScreenPointIntInjected.address == 0)
         _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 2);
-    if (_WorldToScreenPointIntInjected.get() == 0)
+    if (_WorldToScreenPointIntInjected.address == 0)
         _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 3);
 
     _Camera = CameraClass.dump("get_main", 0);
