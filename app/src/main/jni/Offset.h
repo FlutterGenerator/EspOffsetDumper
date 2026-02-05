a4aa0dd6bc309748240d2fdd9fe9b3729bd692ff
tojik_proof_93 <elementman50@yandex.com>
2026-02-06 04:05:05 +0500

    Update Offset.h

--- app/src/main/jni/Offset.h
+++ app/src/main/jni/Offset.h
@@ -1,16 +1,16 @@
-lib l2cpp;
-Address _Transform, _GetPositionInjected, _WorldToScreenPointIntInjected, _Camera;
+NepSymbol::LibManager lib; // создаём объект
+NepSymbol::Address _Transform, _GetPositionInjected, _WorldToScreenPointIntInjected, _Camera;
 
 void getAddress() {
     lib.name = "libil2cpp.so";
     lib.open();
 
     // Component
-    Uclass ComponentClass = lib.getClass("UnityEngine", "Component");
+    NepSymbol::Uclass ComponentClass = lib.getClass("UnityEngine", "Component");
     _Transform = ComponentClass.dump("get_transform", 0);
 
     // Transform
-    Uclass TransformClass = lib.getClass("UnityEngine", "Transform");
+    NepSymbol::Uclass TransformClass = lib.getClass("UnityEngine", "Transform");
     _GetPositionInjected = TransformClass.dump("get_position_Injected", 0);
     if (_GetPositionInjected.address == 0)
         _GetPositionInjected = TransformClass.dump("get_position_Injected", 1);
@@ -20,7 +20,7 @@ void getAddress() {
         _GetPositionInjected = TransformClass.dump("get_position_Injected", 3);
 
     // Camera
-    Uclass CameraClass = lib.getClass("UnityEngine", "Camera");
+    NepSymbol::Uclass CameraClass = lib.getClass("UnityEngine", "Camera");
     _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 0);
     if (_WorldToScreenPointIntInjected.address == 0)
         _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 1);
