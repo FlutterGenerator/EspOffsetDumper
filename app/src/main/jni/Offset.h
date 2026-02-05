514d92493d9eb69516190a0319fe9eed6f45fe94
tojik_proof_93 <elementman50@yandex.com>
2026-02-06 04:28:50 +0500

    Update Offset.h

--- app/src/main/jni/Offset.h
+++ app/src/main/jni/Offset.h
@@ -1,33 +1,45 @@
-NepSymbol::LibManager lib; // создаём объект
-NepSymbol::Address _Transform, _GetPositionInjected, _WorldToScreenPointIntInjected, _Camera;
+lib l2cpp;
+Address _Transform, _GetPositionInjected, _WorldToScreenPointIntInjected, _Camera;
+
+// Дополнительные переменные для теста всех индексов (чтобы видеть, какой рабочий)
+Address posInjected[4];          // для get_position_Injected 0..3
+Address w2sInjected[4];          // для WorldToScreenPoint_Injected 0..3
 
 void getAddress() {
-    lib.name = "libil2cpp.so";
-    lib.open();
+    l2cpp.name = "libil2cpp.so";
+    l2cpp.open();
 
-    // Component
-    NepSymbol::Uclass ComponentClass = lib.getClass("UnityEngine", "Component");
-    _Transform = ComponentClass.dump("get_transform", 0);
+    // Normal Example 
+    Uclass ComponentClass = l2cpp.getClass("UnityEngine", "Component");
+    _Transform = ComponentClass.dump("get_transform", 0);  // всегда 0
 
-    // Transform
-    NepSymbol::Uclass TransformClass = lib.getClass("UnityEngine", "Transform");
-    _GetPositionInjected = TransformClass.dump("get_position_Injected", 0);
-    if (_GetPositionInjected.address == 0)
-        _GetPositionInjected = TransformClass.dump("get_position_Injected", 1);
-    if (_GetPositionInjected.address == 0)
-        _GetPositionInjected = TransformClass.dump("get_position_Injected", 2);
-    if (_GetPositionInjected.address == 0)
-        _GetPositionInjected = TransformClass.dump("get_position_Injected", 3);
+    Uclass TransformClass = l2cpp.getClass("UnityEngine", "Transform");
 
-    // Camera
-    NepSymbol::Uclass CameraClass = lib.getClass("UnityEngine", "Camera");
-    _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 0);
-    if (_WorldToScreenPointIntInjected.address == 0)
-        _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 1);
-    if (_WorldToScreenPointIntInjected.address == 0)
-        _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 2);
-    if (_WorldToScreenPointIntInjected.address == 0)
-        _WorldToScreenPointIntInjected = CameraClass.dump("WorldToScreenPoint_Injected", 3);
+    // Все варианты get_position_Injected (0,1,2,3)
+    posInjected[0] = TransformClass.dump("get_position_Injected", 0);
+    posInjected[1] = TransformClass.dump("get_position_Injected", 1);
+    posInjected[2] = TransformClass.dump("get_position_Injected", 2);
+    posInjected[3] = TransformClass.dump("get_position_Injected", 3);
 
-    _Camera = CameraClass.dump("get_main", 0);
+    // Выбери рабочий индекс здесь (раскомментируй один)
+    _GetPositionInjected = posInjected[0];   // ← чаще всего 0 в новых Unity (2023+)
+    // _GetPositionInjected = posInjected[1];
+    // _GetPositionInjected = posInjected[2];
+    // _GetPositionInjected = posInjected[3];
+
+    Uclass CameraClass = l2cpp.getClass("UnityEngine", "Camera");
+
+    // Все варианты WorldToScreenPoint_Injected (0,1,2,3)
+    w2sInjected[0] = CameraClass.dump("WorldToScreenPoint_Injected", 0);
+    w2sInjected[1] = CameraClass.dump("WorldToScreenPoint_Injected", 1);
+    w2sInjected[2] = CameraClass.dump("WorldToScreenPoint_Injected", 2);
+    w2sInjected[3] = CameraClass.dump("WorldToScreenPoint_Injected", 3);
+
+    // Выбери рабочий индекс здесь (раскомментируй один)
+    _WorldToScreenPointIntInjected = w2sInjected[3];   // ← часто 1 или 3
+    // _WorldToScreenPointIntInjected = w2sInjected[1];
+    // _WorldToScreenPointIntInjected = w2sInjected[0];
+    // _WorldToScreenPointIntInjected = w2sInjected[2];
+
+    _Camera = CameraClass.dump("get_main", 0);  // всегда 0
 }
